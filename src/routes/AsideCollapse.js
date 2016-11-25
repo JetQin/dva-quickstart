59937ee11d76e1f62424deae5b99d994811c2a27
Jet <qinjet@gmail.com>
2016-11-25 18:30:35 +0800

    fix bug

--- src/routes/AsideCollapse.js
+++ src/routes/AsideCollapse.js
@@ -1,4 +1,4 @@
-import { Menu, Breadcrumb, Icon } from 'antd';
+import { Menu, Breadcrumb, Icon, Button } from 'antd';
 import React from "react";
 import styles from "./AsideCollapse.css";
 
@@ -20,48 +20,32 @@ const AsideCollapse = React.createClass({
     return (
       <div className={collapse ? "ant-layout-aside ant-layout-aside-collapse" : "ant-layout-aside"}>
         <aside className="ant-layout-sider">
-          <div className="ant-layout-logo"></div>
+          <div className="ant-layout-logo">
+               <Button type="primary">
+                  <Icon type="cloud" />Cloud
+               </Button>
+          </div>
           <Menu mode="inline" theme="dark" defaultSelectedKeys={['user']}>
             <Menu.Item key="user">
-              <Icon type="user" /><span className="nav-text">导航一</span>
+              <Icon type="user" /><span className="nav-text">User</span>
             </Menu.Item>
             <Menu.Item key="setting">
-              <Icon type="setting" /><span className="nav-text">导航二</span>
+              <Icon type="setting" /><span className="nav-text">Setting</span>
             </Menu.Item>
             <Menu.Item key="laptop">
-              <Icon type="laptop" /><span className="nav-text">导航三</span>
+              <Icon type="laptop" /><span className="nav-text">UI</span>
             </Menu.Item>
             <Menu.Item key="notification">
-              <Icon type="notification" /><span className="nav-text">导航四</span>
+              <Icon type="notification" /><span className="nav-text">Notification</span>
             </Menu.Item>
             <Menu.Item key="folder">
-              <Icon type="folder" /><span className="nav-text">导航五</span>
+              <Icon type="folder" /><span className="nav-text">Dash</span>
             </Menu.Item>
           </Menu>
           <div className="ant-aside-action" onClick={this.onCollapseChange}>
             {collapse ? <Icon type="right" /> : <Icon type="left" />}
           </div>
         </aside>
-        <div className="ant-layout-main">
-          <div className="ant-layout-header"></div>
-          <div className="ant-layout-breadcrumb">
-            <Breadcrumb>
-              <Breadcrumb.Item>首页</Breadcrumb.Item>
-              <Breadcrumb.Item>应用列表</Breadcrumb.Item>
-              <Breadcrumb.Item>某应用</Breadcrumb.Item>
-            </Breadcrumb>
-          </div>
-          <div className="ant-layout-container">
-            <div className="ant-layout-content">
-              <div style={{ height: 220 }}>
-                内容区域
-              </div>
-            </div>
-          </div>
-          <div className="ant-layout-footer">
-          Ant Design 版权所有 © 2015 由蚂蚁金服体验技术部支持
-          </div>
-        </div>
       </div>
     );
   },
